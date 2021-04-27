638176e596d8c141fc7112b0e3bb90dacd4340e1
Eduardo Andres Peña Rojas - SENA <eapena634@misena.edu.co>
2021-04-27 16:54:41 -0500

    Archivos iniciales para el proyecto

---
+++ Js/sentencias.js
@@ -0,0 +1,10 @@
+function Sentencia() {
+    var CodigoHTML="";
+    for (var i = 1; i < 91; i++) {
+        for(var j = 1; j < 6; j++){
+            CodigoHTML+='<h4 class="mb-3">Secuencia "+i+"</h4><div class="d-block my-3"><div class="custom-control custom-radio"><input id="v'+i+'_'+j+'" name="sequence2" type="radio" class="custom-control-input" required=""><label class="custom-control-label" for="v'+i+'_'+j+'">1. Muy molesta.</label></div><div class="custom-control custom-radio"><input id="v'+i+'_'+j+'" name="sequence2" type="radio" class="custom-control-input" required=""><label class="custom-control-label" for="v'+i+'_'+j+'">2. Molesta.</label></div><div class="custom-control custom-radio"><input id="v'+i+'_'+j+'" name="sequence2" type="radio" class="custom-control-input" required=""><label class="custom-control-label" for="v'+i+'_'+j+'">3. Ligeramente molesta.</label></div><div class="custom-control custom-radio"><input id="v'+i+'_'+j+'" name="sequence2" type="radio" class="custom-control-input" required=""><label class="custom-control-label" for="v'+i+'_'+j+'">4. Perceptible pero no molesta.</label></div><div class="custom-control custom-radio"><input id="v'+i+'_'+j+'" name="sequence2" type="radio" class="custom-control-input" required=""><label class="custom-control-label" for="v'+i+'_'+j+'">5. Imperceptible.</label></div></div><hr class="mb-4">';
+        }
+    }
+    document.getElementById("formulario").innerHTML = CodigoHTML;
+}
+Sentencia();
