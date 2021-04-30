38e1760769622f1a234ee2434322a2b5a1e9b5c8
Eduardo Andres Peña Rojas - SENA <eapena634@misena.edu.co>
2021-04-30 15:20:43 -0500

    Cambio de escalas y funcion extra auxiliar

--- Js/sentencias.js
+++ Js/sentencias.js
@@ -8,3 +8,19 @@ function Sentencia() {
     document.getElementById("formulario").innerHTML = CodigoHTML;
 }
 Sentencia();
+
+function displayRadioValue() {
+    document.getElementById("result").innerHTML = "";
+    var ele = document.getElementsByTagName('input');
+    
+    for(i = 0; i < ele.length; i++) {
+        
+        if(ele[i].type="radio") {
+        
+            if(ele[i].checked)
+                document.getElementById("result").innerHTML
+                        += ele[i].name + " Value: "
+                        + ele[i].value + "<br>";
+        }
+    }
+}
